40cfeb004d2a2535c97ee7ccdeaa09a67d537b3b
Matthew Golden <47402598+mgolden30@users.noreply.github.com>
2024-03-20 13:09:39 -0400

    Add files via upload

--- sparse_regression.m
+++ sparse_regression.m
@@ -3,43 +3,29 @@ This script provides some methods for finding sparse relations from feature
 matrices G
 %}
 
+%% Greedy_regression
 
-%% Combinatoric Search
 addpath('SPIDER_functions/');
 
-number_of_terms = 3;
-%residual_type = "|Gc|"; %Use this residual or the next
-residual_type = "|Gc|/max";
-[models, residuals, cs] = combinatoric_search( G, labels, number_of_terms, scales, residual_type );
-
-%Print to screen best 10 models
-models(1:min(10, numel(models)))
-
-
-
-
-
-%% Greedy_regression
-% Add/remove a term at a time to see how sparsity effects the residual
-
-index = 3; %use 1 2 or 3 depending on your residual choice.
+%Pick a residual to minimize. I have played with three choices, and choice
+%1 and 3 are the most sensible. Choice #2 will likely be deprecated
+%eventually.
 %1: |Gc|
 %2: |Gc|/|G_restricted|
 %3: |Gc|/max|G.*c| <- relative residual
+index = 3;
 
-clf
-if exist('cs_ave')
-  %You've run greedy regression before
-  starting_K = 5;
-  starting_model = cs_ave(:, starting_K);
-else
-  %You have not run greedy regression yet. Start with combinatoric result
-  starting_model = cs(1,:);
-  starting_K = sum( starting_model ~= 0 );
-end
+
+%This choice tells sparse regression to start with the full library and
+%work our way backwards, discarding terms in a greedy fashion.
+starting_model = ones( size(G,2), 1 );
 
 number_of_subsamples = 100; %Do parameter estimation 100 times to estimate uncertainty
 fraction_of_windows_in_subsample = 1/2; %use half of windows randomly each time we estimate parameters
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Start Boilerplate. Probably don't touch this.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %We can pick any of these residuals to minimize
 res1 = @(G,c) norm(G*c)/norm(c);
@@ -53,7 +39,14 @@ res_func_for_sparsification = residual_functions{index};
                                                                    fraction_of_windows_in_subsample, ...
                                                                    residual_functions, ...
                                                                    res_func_for_sparsification );
-                                                      
+          
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% End Boilerplate
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
 %Plot results                                                  
 fs = 16; %Font Size
 lim = size(res_ave,2);
@@ -68,13 +61,24 @@ xlim( [0.5, size(res_ave,2)+0.5] );
 set(gcf,'color','w');
 pbaspect([2 1 1])
 
-xline( starting_K );
-
-%Print some models 
-for interest = 2:6
+%Print some models to screen
+for interest = 2:min(8, size(G,2))
   print_model( interest, cs_ave, res_ave, index, labels, scales )
 end
 
+return;
+
+%% Combinatoric Search
+
+addpath('SPIDER_functions/');
+
+number_of_terms = 3;
+%residual_type = "|Gc|"; %Use this residual or the next
+residual_type = "|Gc|/max";
+[models, residuals, cs] = combinatoric_search( G, labels, number_of_terms, scales, residual_type );
+
+%Print to screen best 10 models
+models(1:min(10, numel(models)))
 
 
 
